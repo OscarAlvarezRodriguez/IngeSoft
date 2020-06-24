7ff9b0da25b7eb999e62cdf407702a5c3c49f590
hazmun2000 <52558171+hazmun2000@users.noreply.github.com>
2020-06-24 03:57:57 -0500

    REPORTES FINALIZADOS

--- UNFarmApp/src/Frontera/FronteraSplash.java
+++ UNFarmApp/src/Frontera/FronteraSplash.java
@@ -90,12 +90,12 @@ public class FronteraSplash extends javax.swing.JPanel implements Runnable {
         do {
             try {
                 tiempo = null;
-                Thread.sleep(3000);
+                Thread.sleep(800);
                 if (regAdmin.registrarAdministrador()) {
                     App.getInstance().ChangePanel(FramePrincipal.INTfronteraRegAdministrador);
                     App.getInstance().framePrincipal.regAdministrador.allSetEmpty();
                 } else if (ddao.verDrogueria()) {
-                    App.getInstance().ChangePanel(FramePrincipal.INTFronteraAutEmpleado);
+                    App.getInstance().ChangePanel(FramePrincipal.INTFronteraMenuAdministrador);
                     App.getInstance().framePrincipal.autenticarEmpleado.allSetEmpty();
                 } else {
                     App.getInstance().ChangePanel(FramePrincipal.INTFronteraRegDrogueria);
